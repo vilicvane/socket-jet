7ecf2d836bd5671c9b7a6998b05fd3b21ceb1a81
vilic <i@vilic.info>
2017-09-17 23:00:14 +0800

    Add jet

---
+++ src/jet.ts
@@ -0,0 +1,100 @@
+import { EventEmitter } from 'events';
+import { Socket } from 'net';
+
+import { Ack, Packet, Parser, build, buildAck } from './packet';
+
+type Resolver = () => void;
+
+export interface JetOptions {
+  timeout?: number;
+}
+
+export class Jet<T> extends EventEmitter {
+  private parser: Parser<T>;
+
+  private lastId = 0;
+  private resolverMap = new Map<number, Resolver>();
+
+  private timeout: number;
+
+  constructor(
+    public socket: Socket,
+    {
+      timeout = 30 * 1000,
+    }: JetOptions = {},
+  ) {
+    super();
+
+    let parser = new Parser<T>();
+
+    socket.on('data', data => parser.append(data));
+
+    parser.on('packet', packet => this.handlePacket(packet));
+    parser.on('ack', ack => this.handleAck(ack));
+    parser.on('error', error => this.emit('error', error));
+
+    this.parser = parser;
+
+    this.timeout = timeout;
+  }
+
+  async send(data: T): Promise<void> {
+    let id = ++this.lastId;
+    let packetBuffer = build(id, data);
+
+    await new Promise((resolve, reject) => {
+      this.socket.write(packetBuffer, (error: any) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve();
+      });
+    });
+
+    let timer: NodeJS.Timer;
+
+    let sentPromise = new Promise<void>(resolve => {
+      this.resolverMap.set(id, () => {
+        clearTimeout(timer);
+        resolve();
+      });
+    });
+
+    let timeoutPromise = new Promise<void>((_resolve, reject) => {
+      timer = setTimeout(() => {
+        reject(new Error('Jet sending timed out'));
+      }, this.timeout);
+    });
+
+    await Promise.race([
+      sentPromise,
+      timeoutPromise,
+    ]);
+  }
+
+  private handlePacket({id, data}: Packet<T>): void {
+    let ackBuffer = buildAck(id);
+
+    this.socket.write(ackBuffer);
+
+    this.emit('data', data);
+  }
+
+  private handleAck({id}: Ack): void {
+    let resolver = this.resolverMap.get(id);
+
+    if (resolver) {
+      resolver();
+    }
+  }
+}
+
+export interface Jet<T> {
+  on(event: 'data', listener: (data: T) => void): this;
+  on(event: 'error', listener: (error: Error) => void): this;
+
+  emit(event: 'data', data: T): boolean;
+  emit(event: 'error', error: Error): boolean;
+}
---
+++ src/test/jet.ts
@@ -0,0 +1,36 @@
+import * as Net from 'net';
+
+import test from 'ava';
+
+import { Jet } from '../jet';
+
+let testData = {test: 123};
+
+test.cb('should send and receive packet', t => {
+  let received = false;
+
+  let server = Net.createServer(socket => {
+    let jet = new Jet(socket);
+
+    jet.on('data', data => {
+      t.deepEqual(data, testData);
+      received = true;
+    });
+  });
+
+  server.listen(() => {
+    let port = server.address().port;
+    let socket = Net.connect(port);
+    let jet = new Jet(socket);
+
+    jet
+      .send(testData)
+      .then(
+        () => {
+          t.true(received);
+          t.end();
+        },
+        t.fail,
+      );
+  });
+});
